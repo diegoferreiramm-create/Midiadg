354a121b5775ab481caf68c491610a2386748b28
diegoferreiramm-create <diegoferreira.mm@gmail.com>
2026-03-30 19:57:02 -0300

    Enhance data capture and display in busca-geral.js

--- sistin/atend/busca-geral.js
+++ sistin/atend/busca-geral.js
@@ -14,23 +14,35 @@ function executarBuscaGeral(tipo) {
         return;
       }
       res.forEach(item => {
+        // Capturar DATA STATUS (coluna N)
         let dStat = "";
         for(let key in item) {
           if(key.toUpperCase().replace(/\s/g,'') === "DATASTATUS") dStat = item[key];
         }
         if(!dStat) dStat = item.dataStatus || item.data_status || item["DATA STATUS"] || "";
+        
+        // Capturar SITUAÇÃO (coluna R)
+        let situacao = item.situacao || item.pagamento || item["SITUACAO"] || item["PAGAMENTO"] || "-";
+        
+        // Capturar PRAZO (coluna S)
+        let prazo = item.prazoPendencia || item.prazo || item["PRAZO"] || "-";
+        
+        // Capturar Nº ARCE (coluna W)
+        let numeroArce = item.numeroArce || item.processo || item["NUMERO_ARCE"] || item["PROCESSO"] || "-";
+        
         div.innerHTML += `
           <div class="res-card">
             <b>NOME:</b> ${item.nome}<br>
             <b>CPF:</b> ${item.cpf} | <b>VIA:</b> ${item.via}<br>
             <b>MUNICÍPIO:</b> ${item.municipio} | <b>PARCEIRO:</b> ${item.parceiro}<br>
-            <b>CARTEIRA:</b> ${item.numCarteira || 'N/A'}<br>
+            <b>CARTEIRA:</b> ${item.numCarteira || item.carteira || 'N/A'}<br>
             <b>STATUS:</b> ${item.status || 'Pendente'} | <b>MOTIVO:</b> ${item.motivo || '-'}<br>
             <b>DATA STATUS:</b> ${dStat}<br>
-            <b>BOLETO:</b> ${item.situacao || item.r || '-'}<br>
-            <b>PRAZO PENDÊNCIA:</b> ${item.prazoPendencia || item.s || '-'}<br>
+            <b>SITUAÇÃO:</b> ${situacao}<br>
+            <b>PRAZO:</b> ${prazo}<br>
+            <b>Nº ARCE:</b> ${numeroArce}<br>
             <b>ATENDENTE:</b> ${item.atendente}<br>
-            <small>Última Atualização: ${item.dataAtu}</small>
+            <small>Última Atualização: ${item.dataAtu || item.dt_atu || '-'}</small>
           </div>
         `;
       });
