39567f143f5fa45afdde573232cc4db8f480d0f6
diegoferreiramm-create <diegoferreira.mm@gmail.com>
2026-04-10 14:40:35 -0300

    Update cadastro.js

--- reserva/cadastro.js
+++ reserva/cadastro.js
@@ -236,8 +236,11 @@ function imprimirProtocolo(id, cpf, nome, nascimento, municipio, via, atendente,
     <body>
       <div class="ticket">
         <div class="header">
-          <h2>PROTOCOLO DE SOLICITAÇÃO</h2>
-          <div class="id-destaque">Nº BOLETO: ${boleto}</div>
+            <h2>PROTOCOLO DE SOLICITAÇÃO</h2>
+            <div class="id-destaque" style="display: flex; justify-content: space-between;">
+                <span>Nº BOLETO: ${boleto}</span>
+                <span>DATA: ${data ? data.split(' ')[0] : ''}</span>
+            </div>
         </div>
         <div class="info-grid">
           <div class="info-item"><b>NOME:</b> ${nome ? nome.toUpperCase() : ''}</div>
